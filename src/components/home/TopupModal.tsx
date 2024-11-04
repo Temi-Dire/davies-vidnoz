69f95ba2e250ba597788bd90e5bfb8f73e3383c3
Davies Dev <davidonzan@gmail.com>
2024-11-04 23:20:54 +0100

    fix buikld

--- src/components/home/TopupModal.tsx
+++ src/components/home/TopupModal.tsx
@@ -1,57 +1,74 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { useGetPaymentChoices, useTopUp } from "@/hooks/usePayment";
 import toast from "react-hot-toast";
 import { Rings } from "react-loader-spinner";
 
 interface TopupModalProps {
-    isTopupOpen: boolean;
-    setIsTopUpOpen: (value: boolean) => void;
+  isTopupOpen: boolean;
+  setIsTopUpOpen: (value: boolean) => void;
 }
 
-type Amount = {price: number | null, method: string | null}
+type Amount = { price: number | null; method: string | null };
 
-const TopupModal:React.FC<TopupModalProps> = ({ isTopupOpen, setIsTopUpOpen }) => {
+const TopupModal: React.FC<TopupModalProps> = ({ isTopupOpen, setIsTopUpOpen }) => {
+  const [selectedAmount, setSelectedAmount] = useState<Amount>({ price: null, method: null });
 
-    const [selectedAmount, setSelectedAmount] = useState<Amount>({price: null, method: null});
+  const paymentChoicesQuery = useGetPaymentChoices();
 
-    const paymentChoicesQuery = useGetPaymentChoices();
+  const topUpQuery = useTopUp();
 
-    const topUpQuery = useTopUp();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isLoading, setIsLoading] = useState(false);
+  const handleTopUp = () => {
+    if (!isLoading) {
+      setIsLoading(true);
+      if (selectedAmount.price && selectedAmount.method) {
+        const { price, method } = selectedAmount;
+        topUpQuery.mutate(
+          { price, method },
+          {
+            onSuccess: (res) => {
+              window.location.href = res.link;
+            },
+          }
+        );
+      } else {
+        toast.error("Please select an amount to top-up");
+      }
+    } else return;
+  };
 
-    const handleTopUp = () => {
-        if (!isLoading) {setIsLoading(true);
-        if(selectedAmount.price && selectedAmount.method) {
-            const {price, method} = selectedAmount;
-            topUpQuery.mutate({price, method}, {onSuccess: (res) => { window.location.href = res.link }})
-        } else {
-            toast.error('Please select an amount to top-up')
-        }} else return;
-    }
-
-    return (
-        <Dialog open={isTopupOpen} onOpenChange={() => setIsTopUpOpen(false) }>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Top-up</DialogTitle>
-                    {/* <DialogDescription> This action cannot be undone. This will permanently delete your account and remove your data from our servers. </DialogDescription> */}
-                </DialogHeader>
-                <div className="flex gap-x-4">
-                    <div className="w-full gap-5 grid grid-cols-3 grid-rows-8">
-                       {paymentChoicesQuery && paymentChoicesQuery?.data && paymentChoicesQuery?.data.map( ({ method, choices }) =>
-                       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                       choices.map(({price, ...rest}, i) =>  <div key={i}
-                       className={` text-center ${ price !== selectedAmount.price ? ' border bg-white text-black' : ' bg-purple-700 text-white '} rounded-lg p-2 border-[#000000] cursor-pointer `}
-                       onClick={() => {setSelectedAmount({ price, method })}}
-                       >
-                        {price} USD
-                    </div>)
-                )}
-                    </div>
-                    {/* <div className="w-full space-y-5">
+  return (
+    <Dialog open={isTopupOpen} onOpenChange={() => setIsTopUpOpen(false)}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Top-up</DialogTitle>
+          {/* <DialogDescription> This action cannot be undone. This will permanently delete your account and remove your data from our servers. </DialogDescription> */}
+        </DialogHeader>
+        <div className="flex gap-x-4">
+          <div className="w-full gap-5 grid grid-cols-3 grid-rows-8">
+            {paymentChoicesQuery &&
+              paymentChoicesQuery?.data &&
+              paymentChoicesQuery?.data.map(({ method, choices }) =>
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                choices.map(({ price }, i) => (
+                  <div
+                    key={i}
+                    className={` text-center ${
+                      price !== selectedAmount.price ? " border bg-white text-black" : " bg-purple-700 text-white "
+                    } rounded-lg p-2 border-[#000000] cursor-pointer `}
+                    onClick={() => {
+                      setSelectedAmount({ price, method });
+                    }}
+                  >
+                    {price} USD
+                  </div>
+                ))
+              )}
+          </div>
+          {/* <div className="w-full space-y-5">
                     {availableOptions.map( ({price, currency}, i) =>
                          currency === 'crypto' &&
                           <div key={i}
@@ -61,12 +78,17 @@ const TopupModal:React.FC<TopupModalProps> = ({ isTopupOpen, setIsTopUpOpen }) =
                             {price} crypto
                         </div>)}
                     </div> */}
+        </div>
+        <Button
+          disabled={Boolean(!(selectedAmount.price && selectedAmount.method))}
+          className="bg-purple-600 text-white hover:bg-purple-700"
+          onClick={handleTopUp}
+        >
+          {!isLoading ? <p>Pay</p> : <Rings height="40" width="40" color="#ffffff" ariaLabel="rings-loading" />}
+        </Button>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-                </div>
-                <Button disabled = {Boolean(!(selectedAmount.price && selectedAmount.method))} className="bg-purple-600 text-white hover:bg-purple-700" onClick={handleTopUp} >{!isLoading ? <p>Pay</p> : <Rings height="40" width="40" color="#ffffff" ariaLabel="rings-loading" />}</Button>
-            </DialogContent>
-        </Dialog>
-    )
-}
-
-export default TopupModal
+export default TopupModal;
